be53b54e1caa394cae449e7e3a46daa2489430a3
Remix Workspace <>
2022-07-27 15:47:40 +0300

    import openzeppelin IERC20.sol and add a depsosit function

--- Contracts/pool.sol
+++ Contracts/pool.sol
@@ -1,8 +1,14 @@
 //SPDX-License-Identifier: Unlicense
  pragma solidity ^0.8.0;
-
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
  contract USDTPool {
+/*
+@notice - Function used to deposit USDT to the pool
+@param - amount - user wants to deposit to the pool
+*/
+function deposit(uint256 amount) public {
 
+}
 
      
  }
