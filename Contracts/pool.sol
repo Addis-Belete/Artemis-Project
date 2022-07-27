0a64b0d08e729766a58097f310fd1d45058ed8ad
Remix Workspace <>
2022-07-27 18:25:35 +0300

    Add constructor and calculate reward function

--- Contracts/pool.sol
+++ Contracts/pool.sol
@@ -10,15 +10,22 @@ function burn(uint256 amount, address from) external;
 }
  contract DAIPool {
      // This contract gives 30% APY for stakers
-  address private DAIAddress = 0x73967c6a0904aA032C103b4104747E88c566B1A2;
+     // Reward calculated per month and added to the user Balance;
+     address predefinedTokenAddress;
+     address receiptTokenAddress;
+   constructor(address _predefinedToken, address _receiptToken) {
+   predefinedTokenAddress = _predefinedToken;
+   receiptTokenAddress = _receiptToken;
+   }  
+  
   struct userInfo {
-      
       uint256 amount;
       uint256 dep_started;
       bool isDeposited;
   }   
-  IERC20 DAI = IERC20(DAIAddress);
-  mapping(address => userInfo) public UserBalance;
+
+  IERC20 DAI = IERC20(predefinedTokenAddress);
+  mapping(address => userInfo) public UserDetails;
 /*
 @notice - Function used to deposit DAI to the pool
 @param - amount - user wants to deposit to the pool
@@ -26,9 +33,31 @@ function burn(uint256 amount, address from) external;
 function deposit(uint256 amount) public {
    require(DAI.balanceOf(msg.sender) > amount, "You don't have enough amount to stake");
    DAI.transfer(address(this), amount);
-   UserBalance[msg.sender] = userInfo(amount, block.timestamp, true); 
-
+   if(UserDetails[msg.sender].isDeposited == true){
+   uint256 initialAmount = UserDetails[msg.sender].amount;
+   uint256 initialTime = UserDetails[msg.sender].dep_started;
+   uint256 reward = calculateReward(initialAmount, initialTime);
+   uint256 finalAmount = initialAmount + amount + reward;
+   UserDetails[msg.sender] = userInfo(finalAmount, block.timestamp, true);
+      }
+   else {
+   UserDetails[msg.sender] = userInfo(amount, block.timestamp, true); 
+   }
+   IreceiptToken(receiptTokenAddress).mint(amount, msg.sender);
 }
 
+/*
+@notice - Function used to calculate rewards;
+@param  - amount - the total DAI deposited at the time of reward calculation
+@param - startTime - the time at which user deposited funds
+*/
+function calculateReward(uint256 amount, uint256 startTime) public view returns(uint256 reward){
+   uint256 one_year = 365 * 24 * 60 * 60;
+   uint256 endTime = block.timestamp;
+   uint256 duration = endTime - startTime;
+   uint256 rewardPercent = ((duration * 3) /100)/one_year;
+   reward = amount * rewardPercent;
+
+}
      
  }
